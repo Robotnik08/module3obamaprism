6402fa0dc1f79965ba938766e5c0f161b97b81cf
Robotnik08 <89689979+Robotnik08@users.noreply.github.com>
2022-11-30 11:12:49 +0100

    Contact Sebastiaan

---
+++ assets/bedankt.php
@@ -0,0 +1,11 @@
+<?php
+    $subject = $_GET["main_titles"];
+    $body = "New message:\nGeneral data:\n===================================\nFull name: " . $_GET["vn"] . " " . $_GET["tv"] . " " . $_GET["an"] . "\nEmail: " . $_GET["email"] . "\nPhonenumber: " . $_GET["number"] . "\nAdress: " . $_GET["adress-place"] . " " . $_GET["adress-adress"] . " " . $_GET["adress-postcode"] 
+    . "\n===============================\n" . $_GET["main"] . "\n===================================";
+
+    if (mail("hendrikhogendijk@gmail.com", $subject, $body, "From: Hogendijk Hoveniers")) {
+        echo "Success";
+    } else {
+        echo "Email failed";
+    }
+?>
